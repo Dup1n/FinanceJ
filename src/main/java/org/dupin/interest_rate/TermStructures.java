c3bc53f3393d9b9b917d1ab902c258bf7f1a5daa
Dup1n <elianklk@gmail.com>
2023-01-20 17:29:21 -0400

    feat: Added spotRate methods to Term Structures

--- src/main/java/org/dupin/interest_rate/TermStructures.java
+++ src/main/java/org/dupin/interest_rate/TermStructures.java
@@ -1,5 +1,9 @@
 package org.dupin.interest_rate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TermStructures {
 
     public double disFromyld(double spotrate, double time) {
@@ -20,5 +24,55 @@ public class TermStructures {
 
     public double forateFromyld(double r1, double r2, double t1, double t2) {
         return (r2 * (t2 / (t2 - t1))) - (r1 * (t1 /(t2 - t1)));
+    }
+
+    public double spotRate(double price, double coupon, List<Double> yields) {
+        int period = 1;
+        double sumFactor = 0.0;
+
+        for (double yield: yields) {
+            sumFactor += 1 / Math.pow(1 + yield, period);
+            period++;
+        }
+
+        return Math.pow(facto(price, coupon, sumFactor, 1), (double) 1 / (period)) - 1 ;
+    }
+
+    public double facto(double price, double coupon, double spots, int periods) {
+        return ((coupon / periods) + 100) / (price - ((coupon / periods) * spots));
+    }
+
+    public List<Double> spotFcoupon(List<List<Double>> pcData) {
+        return spotFcoupon(pcData, 1);
+    }
+
+    public List<Double> spotFcoupon(List<List<Double>> pcData, int periods) {
+        int n = pcData.size();
+        double index = 1.0;
+        List<Double> spots = new ArrayList<>();
+
+        double price = pcData.get(0).get(0) / 100.0;
+        spots.add((100 / pcData.get(0).get(0)) - 1);
+
+        for (int s = 1; s < n; s++) {
+            index++;
+            spots.add(Math.exp(1 / index * Math.log(facto(pcData.get(s).get(0), pcData.get(s).get(1), price, periods))) - 1);
+            price += (Math.exp(-index * Math.log(1 + spots.get(s))));
+        }
+        return spots;
+    }
+
+    public double parCoupon(List<Double> spots,int nperiod)
+    {
+        if(spots.size() == nperiod) {
+            AtomicInteger i = new AtomicInteger(1);
+
+            double finaldisc = (1.0 - (Math.exp(-nperiod * Math.log(1.0 + spots.get(nperiod - 1)))));
+            double flowdisc = spots.stream().reduce(0.0, (acc, spot) -> acc + Math.exp(-i.getAndIncrement() * Math.log(1.0 + spot)));
+
+            return  finaldisc / flowdisc;
+        }
+
+        return -1.0;
     }
 }
--- src/test/java/interest_rate/TermStructuresTest.java
+++ src/test/java/interest_rate/TermStructuresTest.java
@@ -1,7 +1,11 @@
 package interest_rate;
 
 import org.dupin.interest_rate.TermStructures;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +24,45 @@ public class TermStructuresTest {
         assertEquals(0.0512, termStructures.yldFromdisc(0.95, 1), 1e-4);
         assertEquals(0.0961, termStructures.yldFromdisc(0.825, 2), 1e-4);
         assertEquals(0.141, termStructures.forateFromdisc(0.95, 0.825, 1, 2), 1e-4);
+    }
+
+    @Test
+    @DisplayName("nth-Spot Rate with price, coupon and spots rate")
+    void testSpotRate() {
+        termStructures = new TermStructures();
+
+        assertEquals(0.1338, termStructures.spotRate(95, 10, Arrays.asList(0.06)), 1e-4);
+
+        assertEquals(0.0236, termStructures.spotRate(99.735, 2.26, Arrays.asList(0.0128, 0.0183)), 1e-4);
+        assertEquals(0.0324, termStructures.spotRate(99.908, 3.16, Arrays.asList(0.0128, 0.0183, 0.0236, 0.0277)), 1e-4);
+    }
+
+    @Test
+    @DisplayName("Test Spot rate with List of prices and Coupon")
+    void testListSpotRate() {
+        termStructures = new TermStructures();
+
+        List<List<Double>> testData = Arrays.asList(
+                Arrays.asList(98.736, 0.0),
+                Arrays.asList(99.908, 1.78),
+                Arrays.asList(99.735, 2.26),
+                Arrays.asList(99.908, 3.16),
+                Arrays.asList(99.822, 3.67),
+                Arrays.asList(99.675, 4.14),
+                Arrays.asList(98.759, 4.92)
+        );
+
+        List<Double> ansx = termStructures.spotFcoupon(testData);
+
+        assertEquals(0.0183, ansx.get(1), 1e-4);
+        assertEquals(0.0236, ansx.get(2), 1e-4);
+        assertEquals(0.0324, ansx.get(3), 1e-4);
+    }
+
+    @Test
+    @DisplayName("Test Calculate the required Coupon")
+    void testCalCoupon() {
+        termStructures = new TermStructures();
+        assertEquals(0.02343, termStructures.parCoupon(Arrays.asList(0.0128, 0.0183, 0.0236), 3), 1e-4);
     }
 }
